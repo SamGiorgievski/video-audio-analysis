674e723972fcf1205bb3717041aa9381f95bcc32
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-06-22 18:52:26 -0400

    frontpage setup

--- client/src/App.js
+++ client/src/App.js
@@ -12,20 +12,14 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title"> Video Analyzer</h1>
+      <div className="video">
+        <div className="videoboxes">
+          <h3> Video 1</h3>
+          </div>
+        
+
+      </div>
     </div>
   );
 }
